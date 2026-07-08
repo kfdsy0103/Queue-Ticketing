8299b8249bbdd8f442401b4b0c1b2a581b022c6c
pywoo <kfdsy0103@naver.com>
2026-07-08 22:41:12 +0900

    feat: EnterDTO 작성

---
+++ src/main/java/ticketing/domain/queue/dto/EnterDTO.java
@@ -0,0 +1,54 @@
+package ticketing.domain.queue.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ticketing.domain.queue.enums.EnterType;
+
+public class EnterDTO {
+
+	@Getter
+	@NoArgsConstructor
+	public static class Request {
+		Long userId;
+		Long concertScheduleId;
+		EnterType enterType;
+
+		public Command toCommand() {
+			return Command.builder()
+				.userId(userId)
+				.concertScheduleId(concertScheduleId)
+				.enterType(enterType)
+				.build();
+		}
+	}
+
+	@Getter
+	@Builder
+	public static class Command {
+		Long userId;
+		Long concertScheduleId;
+		EnterType enterType;
+	}
+
+	@Getter
+	@Builder
+	public static class Result {
+		String token;
+		boolean needToChoose;
+
+		public Response toResponse() {
+			return Response.builder()
+				.token(token)
+				.needToChoose(needToChoose)
+				.build();
+		}
+	}
+
+	@Getter
+	@Builder
+	public static class Response {
+		String token;
+		boolean needToChoose;
+	}
+}
