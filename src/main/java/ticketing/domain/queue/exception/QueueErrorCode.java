6dd5718fa1801b5ba7ba2bf6b3fa0322c078206e
pywoo <kfdsy0103@naver.com>
2026-07-09 19:30:26 +0900

    feat: QueueErrorCode에 처리된 경우 및 busy 추가

--- src/main/java/ticketing/domain/queue/exception/QueueErrorCode.java
+++ src/main/java/ticketing/domain/queue/exception/QueueErrorCode.java
@@ -11,7 +11,8 @@ import ticketing.global.apiPayload.code.BaseCode;
 public enum QueueErrorCode implements BaseCode {
 
 	INVALID_ENTER_TYPE(HttpStatus.BAD_REQUEST, "QUEUE_400_01", "유효하지 않은 대기열 입장 타입입니다."),
-	NOT_IN_QUEUE(HttpStatus.NOT_FOUND, "QUEUE_404_01", "대기열에 등록되지 않은 회원입니다.");
+	NOT_IN_QUEUE(HttpStatus.NOT_FOUND, "QUEUE_404_01", "대기열에 등록되지 않은 회원입니다."),
+	QUEUE_BUSY(HttpStatus.TOO_MANY_REQUESTS, "QUEUE_429_01", "요청이 몰려 처리 중입니다. 잠시 후 다시 시도해주세요.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
