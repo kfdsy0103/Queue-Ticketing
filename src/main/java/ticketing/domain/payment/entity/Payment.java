3778b8a173570799d2aae6da3dc30eafdf887b64
pywoo <kfdsy0103@naver.com>
2026-07-11 17:38:28 +0900

    feat: 주문 정보 관리를 위한 payment 엔티티 생성

---
+++ src/main/java/ticketing/domain/payment/entity/Payment.java
@@ -0,0 +1,42 @@
+package ticketing.domain.payment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ticketing.domain.order.order.entity.Order;
+import ticketing.global.entity.BaseEntity;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Payment extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	private String tid;
+
+	private String redirectUrl;
+
+	private int totalPrice;
+}
