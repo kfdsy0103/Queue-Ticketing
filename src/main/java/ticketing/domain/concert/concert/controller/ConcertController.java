5dd7c8217f23e1b48d6943510a6b5605026d39cc
pywoo <kfdsy0103@naver.com>
2026-07-13 16:10:28 +0900

    fix: 로깅 argument 순서 오류 수정

--- src/main/java/ticketing/domain/concert/concert/controller/ConcertController.java
+++ src/main/java/ticketing/domain/concert/concert/controller/ConcertController.java
@@ -35,7 +35,7 @@ public class ConcertController {
 
 	@GetMapping("/{concertId}")
 	public CommonResponse<FindDTO.Response> find(@PathVariable Long concertId, @RequestParam Long userId) {
-		log.info("[ConcertController] find() 호출 - userId: {}, concertId: {}", concertId, userId);
+		log.info("[ConcertController] find() 호출 - userId: {}, concertId: {}", userId, concertId);
 		FindDTO.Result result = concertService.find(FindDTO.Command.builder().concertId(concertId).build());
 		return CommonResponse.onSuccess(GeneralSuccessCode.OK, result.toResponse());
 	}
