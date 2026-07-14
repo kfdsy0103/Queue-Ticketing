820f9b675793f8f55f0cfe6455cd5b8cb209a451
pywoo <kfdsy0103@naver.com>
2026-07-15 00:02:35 +0900

    feat: MDC TaskDecorator 연결

--- src/main/java/ticketing/global/config/AsyncConfig.java
+++ src/main/java/ticketing/global/config/AsyncConfig.java
@@ -7,9 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import lombok.RequiredArgsConstructor;
+import ticketing.global.mdc.MDCTaskDecorator;
+
 @EnableAsync
 @Configuration
+@RequiredArgsConstructor
 public class AsyncConfig {
+
+	private final MDCTaskDecorator mdcTaskDecorator;
 
 	@Bean(name = "queueTaskExecutor")
 	public Executor queueTaskExecutor() {
@@ -18,6 +24,7 @@ public class AsyncConfig {
 		executor.setMaxPoolSize(8);
 		executor.setQueueCapacity(100);
 		executor.setThreadNamePrefix("queue-worker-");
+		executor.setTaskDecorator(mdcTaskDecorator);
 		executor.initialize();
 		return executor;
 	}
