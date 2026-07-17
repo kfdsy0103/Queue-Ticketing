3c82fc4956d30c1e5b12ffb381cac8c96ab97d09
pywoo <kfdsy0103@naver.com>
2026-07-18 00:21:52 +0900

    feat: 헬스체크 용 API 작성

--- src/main/java/ticketing/Application.java
+++ src/main/java/ticketing/Application.java
@@ -1,21 +1,30 @@
 package ticketing;
 
+import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.annotation.PostConstruct;
 
 @EnableRetry
 @EnableScheduling
+@RestController
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/health")
+	public String health() {
+		return LocalDateTime.now().toString();
 	}
 
 	@PostConstruct
