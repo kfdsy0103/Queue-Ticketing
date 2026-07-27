a6a1f98d2b578e1bf01e484c79cc741a02d9ca47
pywoo <kfdsy0103@naver.com>
2026-07-27 22:10:28 +0900

    feat: payment index

--- src/main/java/ticketing/domain/payment/entity/Payment.java
+++ src/main/java/ticketing/domain/payment/entity/Payment.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -21,7 +22,10 @@ import ticketing.domain.order.order.entity.Order;
 import ticketing.global.entity.BaseEntity;
 
 @Entity
-@Table(name = "payments")
+@Table(
+	name = "payments",
+	indexes = @Index(name = "idx_payments_status_created_at", columnList = "status, created_at")
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
