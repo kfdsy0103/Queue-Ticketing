4040725a4a481d8f5fbf6f169f2c75880b930776
pywoo <kfdsy0103@naver.com>
2026-08-06 15:39:48 +0900

    docs: occupy Key의 TTL 연장 관련 주석 제거
    주문 create() 시 occupy TTL을 5분 연장하던 로직이 제거되어 더 이상 유효하지 않은 설명을 삭제한다.

--- src/main/java/ticketing/domain/concert/scheduleseat/constants/ScheduleSeatRedisKeys.java
+++ src/main/java/ticketing/domain/concert/scheduleseat/constants/ScheduleSeatRedisKeys.java
@@ -6,7 +6,6 @@ public final class ScheduleSeatRedisKeys {
 
 	/**
 	 * 좌석 점유(선점) 여부를 나타내는 Key. (value: 점유한 userId)
-	 * 최초 occupy() 점유 시 5분 TTL로 시작하고, 찰나의 race condition을 막기 위해 주문 craete() 시 TTL이 한번 연장된다.
 	 */
 	public static String occupyKey(Long scheduleSeatId) {
 		return "occupy:scheduleSeat:" + scheduleSeatId;
