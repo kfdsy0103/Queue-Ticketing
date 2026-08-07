84358785bb069c6c187d1edcc7515f64f7e8acd5
pywoo <kfdsy0103@naver.com>
2026-08-07 22:46:25 +0900

    feat: 좌석 상태 Enum에 관한 간단한 설명 추가

--- src/main/java/ticketing/domain/concert/scheduleseat/enums/SeatDisplayStatus.java
+++ src/main/java/ticketing/domain/concert/scheduleseat/enums/SeatDisplayStatus.java
@@ -1,10 +1,17 @@
 package ticketing.domain.concert.scheduleseat.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * DTO 표시용
  */
+@Getter
+@RequiredArgsConstructor
 public enum SeatDisplayStatus {
-    AVAILABLE,
-    OCCUPIED,
-    SOLD
+    AVAILABLE("예약 가능한 좌석"),
+    OCCUPIED("다른 사용자가 점유 중인 좌석"),
+    SOLD("결제가 완료되어 판매된 좌석");
+
+    private final String description;
 }
