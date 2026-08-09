f2bcffc8756634ba750e3f304f5908103e2da35e
pywoo <kfdsy0103@naver.com>
2026-08-09 22:32:50 +0900

    feat: Cache 관련 metric 수집용 constants 패키지 분리

---
+++ src/main/java/ticketing/global/cache/constants/CacheMetric.java
@@ -0,0 +1,10 @@
+package ticketing.global.cache.constants;
+
+public final class CacheMetric {
+	public static final String CACHE_GETS_METRIC = "ticketing.cache.gets";
+	public static final String TAG_GROUP = "group";
+	public static final String TAG_RESULT = "result";
+	public static final String RESULT_LOCAL_HIT = "local_hit";
+	public static final String RESULT_GLOBAL_HIT = "global_hit";
+	public static final String RESULT_MISS = "miss";
+}
