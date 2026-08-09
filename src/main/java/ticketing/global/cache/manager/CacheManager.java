16937bab5ccdf4a7865cf38dbd9e95bb943710df
pywoo <kfdsy0103@naver.com>
2026-08-09 21:09:00 +0900

    feat: cacheManager 인터페이스 정의

---
+++ src/main/java/ticketing/global/cache/manager/CacheManager.java
@@ -0,0 +1,33 @@
+package ticketing.global.cache.manager;
+
+import ticketing.global.cache.CacheWrapper;
+import ticketing.global.cache.enums.CacheGroup;
+import ticketing.global.cache.enums.CacheType;
+
+public interface CacheManager {
+
+	/**
+	 * LOCAL or GLOBAL
+	 */
+	CacheType getSupportedType();
+
+	/**
+	 * 캐시 데이터를 조회합니다.
+	 */
+	<T> CacheWrapper<T> get(CacheGroup group, String cacheKey);
+
+	/**
+	 * 캐시 데이터를 적재합니다.
+	 */
+	void set(CacheGroup group, String cacheKey, CacheWrapper<?> value);
+
+	/**
+	 * 남은 TTL을 밀리초로 반환합니다.
+	 */
+	Long getExpire(CacheGroup group, String cacheKey);
+
+	/**
+	 * 캐시 엔트리 하나를 무효화합니다.
+	 */
+	void evict(CacheGroup group, String cacheKey);
+}
